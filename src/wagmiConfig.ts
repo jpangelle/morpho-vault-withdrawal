187d346c22dc38f78f66eec08f571bca0835e4ea
JP Angelle <jp@angelle.me>
2024-08-16 18:17:19 -0500

    Set up Tenderly rpc

--- src/wagmiConfig.ts
+++ src/wagmiConfig.ts
@@ -1,16 +1,17 @@
+import { vMainnet } from "@/tenderly.config";
 import { cookieStorage, createConfig, createStorage, http } from "wagmi";
-import { mainnet, sepolia } from "wagmi/chains";
+import { injected } from "wagmi/connectors";
 
 export function getConfig() {
   return createConfig({
-    chains: [mainnet, sepolia],
+    chains: [vMainnet],
+    connectors: [injected()],
     ssr: true,
     storage: createStorage({
       storage: cookieStorage,
     }),
     transports: {
-      [mainnet.id]: http(),
-      [sepolia.id]: http(),
+      [vMainnet.id]: http(process.env.NEXT_PUBLIC_TENDERLY_RPC_URL),
     },
   });
 }
