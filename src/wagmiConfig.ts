95d4e9e505efb49b9f74783fc8d2db3f5d362411
JP Angelle <jp@angelle.me>
2024-08-17 08:59:41 -0500

    Use mainnet on prod

--- src/wagmiConfig.ts
+++ src/wagmiConfig.ts
@@ -1,17 +1,19 @@
 import { vMainnet } from "@/tenderly.config";
 import { cookieStorage, createConfig, createStorage, http } from "wagmi";
+import { mainnet, sepolia } from "wagmi/chains";
 import { injected } from "wagmi/connectors";
 
 export function getConfig() {
   return createConfig({
-    chains: [vMainnet],
+    chains: [mainnet, sepolia, vMainnet],
     connectors: [injected()],
     ssr: true,
     storage: createStorage({
       storage: cookieStorage,
     }),
     transports: {
-      [vMainnet.id]: http(process.env.NEXT_PUBLIC_TENDERLY_RPC_URL),
+      [1]: http(process.env.NEXT_PUBLIC_TENDERLY_RPC_URL),
+      [sepolia.id]: http(),
     },
   });
 }
