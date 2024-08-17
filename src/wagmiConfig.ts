f5b03a5e1c1beeeca3357f81d3a23a537f557c62
JP Angelle <jp@angelle.me>
2024-08-17 13:00:46 -0500

    Remove unnecessary files

--- src/wagmiConfig.ts
+++ src/wagmiConfig.ts
@@ -1,11 +1,10 @@
-import { vMainnet } from "@/tenderly.config";
 import { cookieStorage, createConfig, createStorage, http } from "wagmi";
-import { mainnet, sepolia } from "wagmi/chains";
+import { mainnet } from "wagmi/chains";
 import { injected } from "wagmi/connectors";
 
 export function getConfig() {
   return createConfig({
-    chains: [mainnet, sepolia, vMainnet],
+    chains: [mainnet],
     connectors: [injected()],
     ssr: true,
     storage: createStorage({
@@ -13,7 +12,6 @@ export function getConfig() {
     }),
     transports: {
       [1]: http(process.env.NEXT_PUBLIC_TENDERLY_RPC_URL),
-      [sepolia.id]: http(),
     },
   });
 }
